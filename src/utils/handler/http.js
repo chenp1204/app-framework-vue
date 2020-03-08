84048da879975e560670aeb1ce2392b987c4d6e6
chenp1204 <361291408@qq.com>
2020-03-08 16:10:10 +0800

    http封装请求逻辑修改

--- src/utils/handler/http.js
+++ src/utils/handler/http.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import axios from 'axios'
 import store from '@/store'
 import { ACCESS_TOKEN } from '@/store/mutation-types'
-import notification from 'ant-design-vue/es/notification'
+import messager from 'ant-design-vue/es/message'
 import { Promise } from 'q'
-
+import { apiPrefix, isContainer } from '@/config'
+import { userJumper } from '@/views/jumper'
 // 创建axios实例
 const instance = axios.create({
-  baseURL: process.env.VUE_APP_API_BASE_URL, // api base_url // api的base_url
+  // eslint-disable-next-line no-undef
+  baseURL: apiPrefix, // api base_url // api的base_url
   timeout: 10000 // 请求超时时间
 })
 
@@ -18,14 +20,14 @@ const errorHandler = (error) => {
     // 处理http响应错误
     const response = error.response
     switch (response.status) {
-      case 401:
-      case 408: {
-        notification.error('登录信息无效或Token已过期,请重新登录')
-        store.dispatch('Logout').then(() => {
-          setTimeout(() => {
-            window.location.reload()
-          }, 1500)
-        })
+      case 401: {
+        messager.error(response.data.message)
+        store.dispatch('Logout')
+          .then(() => {
+            setTimeout(() => {
+              userJumper.goLogin()
+            }, 500)
+          })
         return
       }
       default:
@@ -34,15 +36,15 @@ const errorHandler = (error) => {
     }
   } else if (error.request) {
     // 处理http请求错误
-    errorMsg = error.request.message
+    errorMsg = error.request.statusText || error.message
   } else {
     // 处理其他错误
-    errorMsg = error.message
+    errorMsg = error.message || error
   }
 
   // 默认错误处理，弹窗错误消息
   if (errorMsg) {
-    notification.error(errorMsg)
+    messager.error(errorMsg)
   }
 
   return Promise.reject(new Error(errorMsg))
@@ -52,11 +54,11 @@ const errorHandler = (error) => {
 instance.interceptors.request.use(
   config => {
     // 添加请求token
-    const userToken = Vue.ls.get(ACCESS_TOKEN)
-    const userId = sessionStorage.getItem('userId')
-    if (userToken) {
-      config.headers.common['Authorization'] = `${userId}_${userToken}`
+    if (!isContainer) {
+      const userToken = Vue.ls.get(ACCESS_TOKEN)
+      config.headers.common['Authorization'] = userToken
     }
+
     return config
   },
   errorHandler
@@ -66,28 +68,29 @@ instance.interceptors.request.use(
 instance.interceptors.response.use(
   response => {
     // 只解析返回的json数据，非json数据直接返回整个响应
-    // const contentType = response.headers['content-type'].toLowerCase()
-    // if (contentType.includes('application/json')) {
-    //   // 处理json数据
-    //   const result = response.data
-    //   if (result.code === '0000') {
-    //     const data = result.data
-    //     return data
-    //   } else {
-    //   // 接口逻辑错误
-    //     const error = {
-    //       response: {
-    //         status: result.code,
-    //         statusText: result.message
-    //       }
-    //     }
-    //     return errorHandler(error)
-    //   }
-    // } else {
-    //   // 非json数据直接返回
-    //   return response
-    // }
-    return response.data
+    const contentType = response.headers['content-type']
+    if (contentType) {
+      if (contentType.toLowerCase().includes('application/json')) {
+        // 处理json数据
+        const result = response.data
+        if (result.code === '00000000') {
+          return result.data
+        } else {
+          // 接口逻辑错误
+          return errorHandler({
+            response: {
+              status: result.code,
+              statusText: result.message
+            }
+          })
+        }
+      } else {
+        // 非json数据直接返回
+        return response
+      }
+    } else {
+      return response.data || response
+    }
   },
   errorHandler
 )
